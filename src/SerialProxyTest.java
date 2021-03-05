50fc9b011f717d3d3d521fad4f1c3873c0f820a1
lpower <lpowerexan@outlook.com>
2021-03-05 07:55:06 +0400

    Initial commit

---
+++ src/SerialProxyTest.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+
+
+public class SerialProxyTest {
+
+    public static void main(String[] args) {
+        String fileName = "data.ser";
+
+        Data data = new Data("WilliamP");
+
+        try {
+            SerializationUtil.serialize(data, fileName);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Data newData = (Data) SerializationUtil.deserialize(fileName);
+            System.out.println(newData);
+        } catch (ClassNotFoundException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
